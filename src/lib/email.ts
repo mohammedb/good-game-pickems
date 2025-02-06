7726f8adbaa156eb7d860e0bf92791afd423cc51
mohammedb <mohammedbarzinje@gmail.com>
2025-02-06 01:16:48 +0100

    email

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,6 +4,12 @@ if (!process.env.RESEND_API_KEY) {
   throw new Error('RESEND_API_KEY is not defined')
 }
 
+// Make sure we use the correct site URL with www and https
+const SITE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://www.ggwp.no'
+    : process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 type SendEmailParams = {
@@ -17,7 +23,7 @@ export async function sendEmail({
   to,
   subject,
   html,
-  from = 'Good Game Pickems <no-reply@goodgamepickems.com>',
+  from = `GGWP.no <no-reply@${new URL(SITE_URL).hostname}>`,
 }: SendEmailParams) {
   try {
     const data = await resend.emails.send({
