593e3c0fb9ba4f8717541d6af887c929fad0eaad
mohammedb <mohammedbarzinje@gmail.com>
2025-07-23 11:45:06 +0200

    Update email-templates.tsx

--- src/lib/email-templates.tsx
+++ src/lib/email-templates.tsx
@@ -59,6 +59,7 @@ function BaseTemplate({ children, previewText }: BaseTemplateProps) {
                         marginBottom: '30px',
                       }}
                     >
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
                       <img
                         src="https://www.ggwp.no/images/logo.png"
                         alt="GGWP.no"
